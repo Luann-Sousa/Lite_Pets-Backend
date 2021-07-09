d77a85401915ae7e56d0870e8245a7363eb00fc6
Luann244 <weversonluan17@gmail.com>
2021-07-09 03:57:24 -0300

    pronto para comecar projeto

--- src/server.ts
+++ src/server.ts
@@ -9,4 +9,4 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
-app.listen(3333, () => console.log("Start is runing !") );
+app.listen(3333, () => console.log(" Start is runing !") );
