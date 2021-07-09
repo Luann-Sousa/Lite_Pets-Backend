705f733fca0ba67f59d35489e37a09172bf4c06a
Luann244 <weversonluan17@gmail.com>
2021-07-09 04:35:47 -0300

    create:migration MyDogs

---
+++ src/database/migrations/1625815240512-CreateMyDog.ts
@@ -0,0 +1,42 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateMyDog1625815240512 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "dogs",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true,
+                    },
+                    {
+                        name: "name",
+                        type: "varchar"
+                    },
+                    {
+                        name: "raca",
+                        type: "varchar"
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestap",
+                        default: "now()",
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestap",
+                        default: "now()",
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("dogs")
+    }
+
+}
